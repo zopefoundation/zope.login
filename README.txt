f69037bc1c3c9b99c0aa88c3f8d09c9a6503f0d5
Hanno Schlichting <hanno@hannosch.eu>
2009-12-31 16:32:53 +0000

    Boilerplate

---
+++ README.txt
@@ -0,0 +1,5 @@
+Overview
+========
+
+This package provides a login helpers for zope.publisher based on the concepts
+of zope.authentication.
---
+++ setup.py
@@ -0,0 +1,54 @@
+##############################################################################
+#
+# Copyright (c) 2006-2009 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(name='zope.login',
+      version = '1.0dev',
+      author='Zope Corporation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Login helpers for zope.publisher / authentication.',
+      long_description=(
+          read('README.txt')
+          + '\n' +
+          read('CHANGES.txt')
+          ),
+      keywords = "zope login authentication",
+      classifiers = [
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope3'],
+      url='http://pypi.python.org/pypi/zope.login',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['zope'],
+      install_requires=['setuptools',
+                        'zope.authentication',
+                        'zope.interface',
+                        'zope.publisher',
+                        ],
+      include_package_data = True,
+      zip_safe = False,
+      )
