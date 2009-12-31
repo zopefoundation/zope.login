a9f4f777da46341d39055a76357708cb777b4442
Hanno Schlichting <hanno@hannosch.eu>
2009-12-31 17:02:19 +0000

    Prepare zope.login 1.0.0.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.login',
-      version = '1.0dev',
+      version = '1.0.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Login helpers for zope.publisher / authentication.',
