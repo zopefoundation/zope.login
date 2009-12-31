8a723465b035c09c70c548c971cb2d261a4627de
Hanno Schlichting <hanno@hannosch.eu>
2009-12-31 17:02:56 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.login',
-      version = '1.0.0',
+      version = '1.0.1dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Login helpers for zope.publisher / authentication.',
