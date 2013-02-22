848846ff173a3c9977c3eee038f85b3532c2be8b
Andrey Lebedev <andrey@lebedev.lt>
2013-02-22 17:50:20 +0200

    Releasing 2.0.0a1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.login',
-      version = '2.0.0dev',
+      version = '2.0.0a1',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Login helpers for zope.publisher / authentication.',
