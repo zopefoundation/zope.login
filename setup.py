5ea818df6c15bd05f66d77b074921a4b99dd43de
Jason Madden <jamadden@gmail.com>
2017-09-01 08:25:28 -0500

    Back to development: 2.1.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*rnames):
 
 setup(
     name='zope.login',
-    version='2.1.0',
+    version='2.1.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Login helpers for zope.publisher / authentication.',
